9d36368ba90a8e6f32c2a9a954b58aaeb3ea5316
Mark Foulger <mark.foulger@hotmail.com>
2021-03-21 21:28:15 +0000

    Complete

--- script.js
+++ script.js
@@ -9,23 +9,27 @@ setInterval(() => {
 }, blinkTimeMS);
 
 //
-// Calcuator functions
+// Calculator functions
 //
 
 function add(x, y) {
-    return x + y;
+    return +x + +y;
 }
 
 function subtract(x, y) {
-    return x - y;
+    return +x - +y;
 }
 
 function multiply(x, y) {
-    return x * y;
+    return +x * +y;
 }
 
 function divide(x, y) {
-    return x + y;
+    if (+y == 0) {
+        alert('Oi! do not divide by zero');
+        return '';
+    } 
+    return Number((+x / +y).toFixed(4));;
 }
 
 function operate(operator, x, y) {
@@ -40,6 +44,7 @@ function operate(operator, x, y) {
             return divide(x,y);
         default:
             console.error('Invalid operation');
+            return '';
     }
 }
 
@@ -51,6 +56,7 @@ const numeralButtons = document.querySelectorAll('.numeral_button');
 const operatorButtons = document.querySelectorAll('.operator_button');
 const clearButton = document.querySelector('#clear_button');
 const backspaceButton = document.querySelector('#backspace_button')
+const equalsButton = document.querySelector('.equals_button');
 const display = document.querySelector('#display_text');
 
 function clearInput() {
@@ -58,26 +64,47 @@ function clearInput() {
 }
 
 function backspaceInput() {
-    if (display.textContent.length > 1) {
-        const numLength = display.textContent.length;
-        display.textContent =  display.textContent.slice(0, numLength - 1);
+    const [x, operator, y] = display.textContent.split(' ');
+    if (operator === undefined) {
+        display.textContent = x.substring(0, x.length - 1);
     } else {
-        display.textContent = '';
+        if (y === '') {
+            display.textContent = x;
+        } else {
+            const len = display.textContent.length;
+            display.textContent = display.textContent.substring(0, len - 1);
+        }
     }
 }
 
 function handleNumberInput(numeral) {
-    if (numeral === '.') {
-        if (!display.textContent.includes('.')){
-            display.textContent = display.textContent + numeral;
-        }
-    } else if (!isNaN(numeral)) {
-        display.textContent = display.textContent + numeral;
+    const [x, operator, y] = display.textContent.split(' ');
+    const currentDigit = operator === undefined ? x : y;
+    if (numeral === '.' && currentDigit.includes('.')) {
+        return;
+    }
+    display.textContent = display.textContent + numeral;
+}
+
+function handlerOperatorInput(op) {
+    const [x, operator, y] = display.textContent.split(' ');
+    if (operator === undefined) {
+        display.textContent += ` ${op} `;
     } else {
-        console.error('Invalid numeral input');
+        if (y === '') {
+            display.textContent = x + ` ${op} `;
+        } else {
+            parseCalculation();
+        }
     }
 }
 
+function parseCalculation() {
+    const [x, operator, y] = display.textContent.split(' ');
+    if (y && y !== '') {
+        display.textContent = operate(operator, x, y);
+    }
+}
 
 //
 // Event Listeners
@@ -87,17 +114,26 @@ clearButton.addEventListener('click', clearInput);
 
 backspaceButton.addEventListener('click', backspaceInput);
 
+equalsButton.addEventListener('click', parseCalculation);
+
 numeralButtons.forEach(numeralButton => {
     numeralButton.addEventListener('click', e => handleNumberInput(e.target.value));
 });
 
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', e => handlerOperatorInput(e.target.dataset.operator))
+});
+
 document.addEventListener('keydown', e => {
+    e.preventDefault();
     if (e.key.match(/[0-9]|\./)) { // contains 0-9 or full stop
-        handleNumberInput(e.key);1
-    }
-    if (e.keyCode === 8) { // backspace button
-        e.preventDefault();
+        handleNumberInput(e.key);
+    } else if (e.key === 'Backspace') { // backspace button
         backspaceInput();
-    } 
+    } else if (e.key.match(/\+|\-|\*|\//)) { // +, -, /, * operator buttons
+        handlerOperatorInput(e.key);
+    } else if (e.key ==='Enter') {
+        parseCalculation();
+    }
 });
     
